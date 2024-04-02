f9c0f99e731f40ddeed7d609a64f2902ffa642ae
Andranik <andran1401@gmail.com>
2024-04-02 22:03:29 +0400

    finish

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Main{
+    public static void main(String[] args) {
+         List<MyNumber> num=new ArrayList<>();
+        num.add(new MyNumber(5,5,4,6));
+       int calc = (int) num.stream().flatMapToInt(MyNumber-> IntStream.of(MyNumber.getValue()))
+               .filter(n->n%2==0)
+               .average()
+               .orElse(0);
+
+        System.out.println("Среднее значение четных чисел: " + calc);
+
+
+    }
+    }
---
+++ MyNumber.java
@@ -0,0 +1,9 @@
+public class MyNumber {
+    private final int[] values;
+
+    public MyNumber(int... values ){
+        this.values=values;
+    }
+
+    public int[] getValue(){return values;}
+}
